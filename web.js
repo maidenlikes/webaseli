8898f43c89254281981e5066fee376a6ffa00035
root <root@ip-172-31-30-4.ap-southeast-1.compute.internal>
2025-05-21 19:19:39 +0000

    Initial commit

---
+++ web.js
@@ -0,0 +1,29 @@
+function mulaiGame() {
+  const TahunBenar = 2007;
+  let tryCount = 3; // 'try' adalah keyword, jadi ganti nama variabel
+  let JawabanBenar = false;
+
+  for (let i = 1; i <= tryCount; i++) {
+    let Jawaban = prompt(`Percobaan ke-${i}: Kapan Terakhir Kali Emyu Menang UCL CIK`);
+
+    if (Jawaban === null) {
+      alert("Kamu Karbit Banget Anjim");
+      break;
+    }
+
+    let tebakan = Number(Jawaban);
+
+    if (isNaN(tebakan)) {
+      alert("Koe Gak Input Nomor Blok Ulangi!");
+      break;
+    }
+
+    if (tebakan === TahunBenar) {
+      alert("Fens Emyu Sejati");
+      JawabanBenar = true;
+      break;
+    } else {
+      alert("Jancok Karbit");
+    }
+  }
+}
